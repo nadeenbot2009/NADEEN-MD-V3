830a61e7d48f95a11608af2c60974958665d8c4b
Nadeen Poorna <nadeenpoornaaluth@proton.me>
2025-03-16 17:02:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,12 +5,13 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "P3YWmQxL#V2OyVt8EBHRiW5IiUKGNdoxtVnEZp_q8ljvitwJZF4g", //put your session id
+SESSION_ID: process.env.SESSION_ID || "XVoxUbYT#TKvbGqKDHwedkv86uC7UVRXSc3MtUisgCWl0dvDfXA0", //put your session id
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/2nmi9q.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "*👋 Hello I am nadeen poorna*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",  //true or false
-MODE: process.env.MODE || "groups", //private or publics
+MODE: process.env.MODE || "public", //private or publics
 AUTO_VOICE: "false", //true or false
+INBOX_BLOCK: process.env.INBOX_BLOCK || "true", //true or false
 MONGODB: "mongodb+srv://lithumhelitha2008:7rDwXUVeTnIfmnjl@cluster0.1d0bq.mongodb.net/",
 CAPTION: "> *🎬NADEEN-MD🎬*",
 JIDS: [],
